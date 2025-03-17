0bab23ab9e9cb564882e07eacdcb6389348e88b0
jiinp2 <jiinpark.work@gmail.com>
2025-03-17 11:45:58 -0400

    Multer storage setup.

---
+++ routes/upload.js
@@ -0,0 +1,12 @@
+import express from "express";
+import multer from "multer";
+import path from "path";
+
+const router = express.Router();
+
+// Multer storage
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => cb(null, "uploads/"),
+  filename: (req, file, cb) =>
+    cb(null, Date.now() + path.extname(file.originalname)),
+});
