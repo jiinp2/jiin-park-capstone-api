f92b7afadb8ad9e8d74353ce0998e4398b978d77
jiinp2 <jiinpark.work@gmail.com>
2025-03-18 16:28:10 -0400

    Routing fixes

--- routes/upload-routes.js
+++ routes/upload-routes.js
@@ -2,6 +2,7 @@ import express from "express";
 import multer from "multer";
 import path from "path";
 import { uploadImages } from "../controllers/upload-controller.js";
+import { getImagesByLogId } from "../controllers/upload-controller.js";
 
 const router = express.Router();
 
