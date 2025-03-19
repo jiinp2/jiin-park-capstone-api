223e3d62f10c63b130637949d44cdd61a49a4612
jiinp2 <jiinpark.work@gmail.com>
2025-03-18 20:36:17 -0400

    Add images fetch

--- controllers/logs-controller.js
+++ controllers/logs-controller.js
@@ -6,12 +6,19 @@ const db = knex(knexConfig);
 export const getLogDetails = async (req, res) => {
   try {
     const { logId } = req.params;
+    console.log("Received logId:", logId);
+
+    if (!logId) {
+      return res.status(400).json({ error: "logId is missing" });
+    }
 
     // Fetch log details
-    const logDetails = await db("logs").where({ id: logId }).first();
+    const logDetails = await db("images").where({ log_id: logId }).first();
+    console.log("Fetched logDetails:", logDetails);
 
     // Fetch images with logId
     const images = await db("images").where({ log_id: logId });
+    console.log("Fetched images:", images);
 
     res.json({ log: logDetails, images });
   } catch (error) {
