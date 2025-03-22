b3f5ae263e7181a6955be3cdc065c987d0649fb7
jiinp2 <jiinpark.work@gmail.com>
2025-03-22 19:56:37 -0400

    Migrations for images

--- migrations/20250322234827_create_images_table.js
+++ migrations/20250322234827_create_images_table.js
@@ -1,15 +1,22 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
-};
+export function up(knex) {
+  return knex.schema.createTable("images", (table) => {
+    table.increments("id").primary();
+    table.string("file_path").notNullable();
+    table.string("file_name").notNullable();
+    table.integer("file_size").notNullable();
+    table.string("file_type").notNullable();
+    table.decimal("latitude", 10, 6);
+    table.decimal("longitude", 10, 6);
+    table.timestamp("timestamp");
+    table
+      .uuid("log_id")
+      .references("log_id")
+      .inTable("logs")
+      .onDelete("CASCADE");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+}
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
-};
+export function down(knex) {
+  return knex.schema.dropTable("images");
+}
