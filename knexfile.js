a73a81256beef1c9923ab7d9d7f15c670ba607cb
jiinp2 <jiinpark.work@gmail.com>
2025-03-22 21:01:08 -0400

    Debugging knex

--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,13 @@ export default {
   client: "mysql2",
   connection: {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
   },
   migrations: {
     tableName: "knex_migrations",
+    directory: "./migrations",
   },
 };
